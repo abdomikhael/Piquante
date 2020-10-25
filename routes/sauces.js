70440a994bdef41fcd3d8122d315ab3b38b1a130
add-new <abdo.mikhael@gmail.com>
2020-10-25 17:28:41 +0100

    likectrl ficher

--- routes/sauces.js
+++ routes/sauces.js
@@ -5,7 +5,7 @@ const Sauce = require('../models/sauce');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 const sauceCtrl = require('../controllers/sauces');
-const likeCtrl = require('../middleware/like-config');
+
 const sauceForm = require('../middleware/sauceform-check')
 const pictureDelete = require('../middleware/formPicture-delete')
 const pictureUpdate = require('../middleware/pictureUpdate')
@@ -16,7 +16,7 @@ router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce, pictureDelete);
 router.put('/:id', auth, multer, pictureUpdate, sauceCtrl.modifySauce, pictureDelete);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
-router.post('/:id/like', auth, likeCtrl, sauceCtrl.likeSauce)
+router.post('/:id/like', auth, sauceCtrl.likeSauce)
 
 
 module.exports = router;
